58f2c81fb1bec8ce59b6c4a173b6b0f70a5ac4b2
boris-volkov <58349574+boris-volkov@users.noreply.github.com>
2026-06-15 10:43:49 -0700

    new games

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/boris/Documents/GitHub/chess_viewer/go_viewer
+# Install script for directory: C:/Users/boris/Documents/GitHub/go_viewer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/boris/Documents/GitHub/chess_viewer/go_viewer/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/boris/Documents/GitHub/go_viewer/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
